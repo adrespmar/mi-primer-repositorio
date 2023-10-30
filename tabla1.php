3e0f99f6ca622da58b513ed01fd35e4cf02c094a
adrespmar <adrespmar@alu.edu.gva.es>
2023-10-30 18:00:24 +0100

    commit tabla php1

---
+++ tabla1.php
@@ -0,0 +1,52 @@
+<html>
+<head>
+<title>Practica 1 PHP</title>
+
+</head>
+<body>
+<h1>Tabla1</h1>
+<?php
+$x = array(); //valores en la horizontal
+$y = array(); //valores en la vertical
+
+//Completar los arrays con los valores correpondientes
+for($i = 0 ; $i< 17; $i++){
+    $y[] = $i;
+}
+
+for($i = 50 ; $i< 61; $i++){
+    $x[] = $i;
+}
+
+$color1 = "Violet";
+$color2 = "Yellow";
+$color3 = "Orange";
+
+//Comienza la tabala
+$tabla = "<table border =\"3\" bgcolor=\"$color1\">";
+
+foreach ($y as $vertical){
+    
+    $tabla.="<tr ".(!$vertical?"bgcolor=\"$color1\"":"bgcolor=\"$color2\"")."><td align=\"center\" bgcolor=\"$color1\">".(!$vertical?"/":$vertical)."</td>";
+    //Si es el primer elemento
+    if($vertical==0){
+        foreach ($x as $horizontal){
+            $tabla.="<td>$horizontal</td>";
+        }
+        $tabla.="</tr>";
+    } //de lo contrario se calcula la razón
+    else{
+        foreach ($x as $horizontal){
+            if($vertical==0) continue;
+            $tabla.="<td  align=\"center\">".($horizontal%$vertical==0?"*":"-")."</td>";
+        }
+        $tabla.="</tr>";
+    }
+
+}
+$tabla.= "</table>";
+echo $tabla;
+?>
+</body>
+</html>
+
